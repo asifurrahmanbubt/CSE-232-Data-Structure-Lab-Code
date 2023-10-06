b1c7cafaa9f032d31feb49bf0f7ba10fc339758f
Asifur Rahman <122219223+asifurrahmanbubt@users.noreply.github.com>
2023-10-06 15:43:53 +0600

    Create Stack Implementation -Push POP operation.py

---
+++ Lab Report 7/Stack Implementation -Push POP operation.py
@@ -0,0 +1,36 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            raise IndexError("Pop from an empty stack")
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            raise IndexError("Peek into an empty stack")
+
+    def size(self):
+        return len(self.items)
+
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+
+print("Stack size:", stack.size())
+print("Top element:", stack.peek())
+
+popped_item = stack.pop()
+print("Popped item:", popped_item)
+print("Stack size after pop:", stack.size())
